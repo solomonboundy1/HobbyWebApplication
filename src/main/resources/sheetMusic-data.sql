19570a00b6ff724bb7ef2a4be376eef83e94f32c
solomonboundy1 <solomonboundy2017@gmail.com>
2021-11-16 15:11:39 +0000

    HWA-15 HWA-16 HWA-17 # controller classes created and package

---
+++ src/main/resources/sheetMusic-data.sql
@@ -0,0 +1,4 @@
+INSERT INTO `sheet_music`
+	(`title`, `author`, `genre`, `price`, `shop_id`)
+values
+	('OSCAR PETERSON: THE VERY BEST OF OSCAR PETERSON: PIANO SOLO', 'Oscar Peterson', 'Jazz', 10.99, 1);
