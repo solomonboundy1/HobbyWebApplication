ea8478ae783bbf9c7a3a48e66134fda9fad88728
solomonboundy1 <solomonboundy2017@gmail.com>
2021-11-16 15:45:47 +0000

    HWA-18 HWA-19 # application properties updated and SQL commands placed in SQL files ready to be assigned

---
+++ src/main/resources/sql-schema.sql
@@ -0,0 +1,28 @@
+DROP SCHEMA `hwa`;
+
+CREATE SCHEMA IF NOT EXISTS `hwa`;
+
+USE `hwa`
+
+DROP TABLE IF EXISTS `shop`;
+DROP TABLE IF EXISTS `sheet_music`;
+
+CREATE TABLE `shop`
+(
+	`shop_id` BIGINT PRIMARY KEY AUTO_INCREMENT,
+	`shop_name` varchar(255) NOT NULL,
+	`shop_address` varchar(255) NOT NULL
+	);
+
+CREATE TABLE `sheet_music`
+(
+	`sheet_music_id` BIGINT PRIMARY KEY AUTO_INCREMENT,
+	`title` varchar(255) NOT NULL,
+	`author` varchar(255) NOT NULL,
+	`genre` varchar(25) NOT NULL,
+	`price` double NOT NULL,
+	`shop_id` bigint NOT NULL
+	);
+	
+	
+	
