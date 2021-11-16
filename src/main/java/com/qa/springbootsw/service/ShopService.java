031ae27a09c659765c3489f8e7d0773bd9ec646d
solomonboundy1 <solomonboundy2017@gmail.com>
2021-11-16 14:39:55 +0000

    HWA-9 HWA-10 HWA-11 # sheet music service created and shop service created

--- src/main/java/com/qa/springbootsw/service/ShopService.java
+++ src/main/java/com/qa/springbootsw/service/ShopService.java
@@ -1,5 +1,46 @@
 package com.qa.springbootsw.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.qa.springbootsw.domain.Shop;
+import com.qa.springbootsw.repo.ShopRepo;
+
+@Service
 public class ShopService {
+	
+	private ShopRepo repo;
+	
+	public ShopService(ShopRepo repo) {
+		this.repo = repo;
+	}
+	
+	public Shop createShop(Shop shop) {
+		
+		return this.repo.saveAndFlush(shop);
+	}
+	
+	public List<Shop> getAll() {
+		return this.repo.findAll();
+	}
+	
+	public Shop getOne(Long id) {
+		return this.repo.findById(id).get();
+	}
+	
+	
+	
+	public Shop update(Long id, Shop shop) {
+		Shop existing = this.repo.findById(id).get();
+		existing.setShopName(shop.getShopName());
+		existing.setAddress(shop.getAddress());
+		return this.repo.saveAndFlush(existing);
+	}
+	
+	public boolean deleteShop(Long id) {
+		this.repo.deleteById(id);
+		return !this.repo.existsById(id); // Bool
+	}
 
 }
